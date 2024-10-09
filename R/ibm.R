2b108d3fac88052e5966688b1f1c81a8f5b3f5b4
milesdwilliams15 <milesdwilliams15@gmail.com>
2024-10-09 13:18:50 -0400

    first push

---
+++ R/ibm.R
@@ -0,0 +1,118 @@
+#' A function to estimate an inverse Burr regression model
+#' 
+#' This function allows you to fit an inverse Burr model of some outcome
+#' either using constant parameter values for the distribution's scale and 
+#' two shape parameters, or else using covariates to fit these parameters.
+#' 
+#' @param outcome The outcome being modeled.
+#' @param mu The scale parameter. Default is a constant formula of `~ 1` or can be the right-hand side of a formula object with covariates.
+#' @param alpha The first shape parameter. Default is a constant formula of `~ 1` or can be the right-hand side of a formula object with covariates.
+#' @param theta The second shape parameter. Default is a constant formula of `~ 1` or can be the right-hand side of a formula object with covariates.
+#' @param data The dataset containing the outcome and any covariates. At the moment, the function cannot handle data with missing values.
+#' @param its The function uses bootstrapping for statistical inference. How many bootstrapped iterations do you want to use? The default is 2000.
+#' @export
+ibm <- function(
+    outcome,     # the outcome
+    mu = ~ 1,    # rhs for mu
+    alpha = ~ 1, # rhs for alpha
+    theta = ~ 1, # rhs for theta
+    data = NULL, # data
+    its = 2000) {# do inference with 2000 bootstraps
+  
+  ## The Data
+  if(!is.null(data)) {
+    y <- data |> dplyr::pull(!!enquo(outcome))
+  } else {
+    y <- outcome
+  }
+  x1 <- model.matrix(mu, data)
+  x2 <- model.matrix(alpha, data)
+  x3 <- model.matrix(theta, data)
+  
+  ## The likelihood
+  inbur_lik <- function(x1, x2, x3, y, pars) {
+    b <- matrix(
+      data = pars[1:(ncol(x1) + ncol(x2) + ncol(x3))],
+      nrow = length(y),
+      ncol = 1
+    )
+    mu <- exp(x1 %*% b[1:ncol(x1)])
+    alpha <- exp(x2 %*% b[ncol(x1) + 1:ncol(x2)])
+    theta <- exp(x3 %*% b[ncol(x1) + ncol(x2) + 1:ncol(x3)])
+    sum(
+      - log(
+        actuar::dinvburr(
+          y, 
+          shape1 = alpha,
+          shape2 = theta,
+          scale = mu
+        )
+      )
+    )
+  }
+  
+  ## Estimation
+  optim(
+    par = rep(0, len = ncol(x1) + ncol(x2) + ncol(x3)),
+    fn = inbur_lik,
+    x1 = x1,
+    x2 = x2,
+    x3 = x3,
+    y = y,
+    hessian = F
+  ) -> opt_out
+  
+  ## Bootstrapping
+  tibble::tibble(
+    its = 1:its,
+    bout = furrr::future_map(
+      its,
+      ~ {
+        bkeep <- sample(1:length(y), length(y), T)
+        optim(
+          par = rep(0, len = ncol(x1) + ncol(x2) + ncol(x3)),
+          fn = inbur_lik,
+          x1 = x1[bkeep, , drop = F],
+          x2 = x2[bkeep, , drop = F],
+          x3 = x3[bkeep, , drop = F],
+          y = y[bkeep],
+          hessian = F
+        ) -> opt_out
+        tibble::tibble(
+          pars = 1:length(opt_out$par),
+          vals = opt_out$par
+        )
+      },
+      .options = furrr::furrr_options(seed = T)
+    )
+  ) |>
+    dplyr::unnest(cols = bout) |>
+    dplyr::group_by(pars) |>
+    dplyr::summarize(
+      std.error = sd(vals, na.rm=T)
+    ) -> boot_se
+  
+  ## Return model output in a tidy tibble
+  list(
+    out = tibble::tibble(
+      term = c(
+        ifelse(rep(ncol(x1) == 1, len = ncol(x1)), 
+               "mu: (Intercept)", 
+               paste0("mu: ", colnames(x1))),
+        ifelse(rep(ncol(x2) == 1, len = ncol(x2)), 
+               "alpha: (Intercept)", 
+               paste0("alpha: ", colnames(x2))),
+        ifelse(rep(ncol(x3) == 1, len = ncol(x3)), 
+               "theta: (Intercept)", 
+               paste0("theta: ", colnames(x3)))
+      ),
+      estimate = opt_out$par,
+      std.error = boot_se$std.error,
+      statistic = estimate / std.error,
+      p.value = 2*pnorm(
+        -abs(statistic)
+      ) |> round(3)
+    ),
+    logLik = -opt_out$value
+  )
+}
---
+++ R/plot.ibm.R
@@ -0,0 +1,159 @@
+#' Plot Pr(X > x) given x for an inverse Burr model
+#' 
+#' This function allows you to plot a 
+#' log-log plot of Pr(X > x) given x by specifying values for the three
+#' main parameters for an inverse Burr distribution (scale, shape 1, and shape 2).
+#' 
+#' @param mu The scale parameter for an inverse Burr model.
+#' @param alpha The first shape parameter for an inverse Burr model.
+#' @param theta The second shape parameter for an inverse Burr model.
+#' @param xmax Default is 1000. The maximum level of x to show in the plot.
+#' @param len Default is 1000. The level of granularity for x.
+#' @param add Are you adding a new inverse Burr model to an existing plot? Default is `FALSE`.
+#' @param legend Default is `NULL`. Do you want to include a legend if adding additional plots? This lets you select the category name for a particular model.
+#' @param legend_title Default is `NULL`. Do you want to provide a custom name for your legend plot?
+#' @export
+ll_plot <- function(
+    mu, alpha, theta, 
+    xmax = 1000, 
+    len = 1000,
+    add = F,
+    legend = NULL,
+    legend_title = NULL
+) {
+  
+  ## X values to be plotted
+  x <- exp(seq(0, log(xmax), len = len))
+  
+  ## The probability that X > x
+  p <- actuar::pinvburr(x, alpha, theta, scale = mu, lower.tail = F)
+  
+  ## Create the plot
+  # ... if the plot is a new plot
+  if(!add) {
+    ggplot2::ggplot() +
+      ggplot2::aes(x, p) +
+      ggplot2::geom_line(
+        ggplot2::aes(
+          color = legend
+        ),
+        size = 1
+      ) +
+      ggplot2::scale_x_log10(
+        labels = scales::comma
+      ) +
+      ggplot2::scale_y_log10(
+        n.breaks = 9,
+        labels = scales::percent
+      ) +
+      ggplot2::labs( 
+        x = "x",
+        y = "Pr(X > x)",
+        color = legend_title
+      ) +
+      ggplot2::theme(
+        panel.grid.major = 
+          ggplot2::element_line(
+            color = "black",
+            linetype = 4,
+            size = .5
+          )
+      ) 
+  } else {
+    # ... if making an addition to the plot
+    ggplot2::last_plot() +
+      ggplot2::geom_line(
+        ggplot2::aes(x, p, color = legend),
+        size = 1
+      ) 
+  }
+}
+#' Plot the fit of an inverse Burr model
+#' 
+#' This function allows you to plot the fit of an inverse Burr model in a 
+#' log-log plot. It accepts the output of an `ibm()` fitted model and 
+#' a required `newdata` object to simulate what Pr(X > x) given x conditional
+#' on values for model covariates. 
+#' 
+#' @param model An object generated via `ibm()`.
+#' @param newdata A dataset with 1 row of values for model covariates.
+#' @param xmax Default is 1000. The maximum level of x to show in the plot.
+#' @param len Default is 1000. The level of granularity for x.
+#' @param add Are you adding a new inverse Burr model to an existing plot? Default is `FALSE`.
+#' @param legend Default is `NULL`. Do you want to include a legend if adding additional plots? This lets you select the category name for a particular model.
+#' @param legend_title Default is `NULL`. Do you want to provide a custom name for your legend plot?
+#' @export
+plot.ibm <- function(
+    model, 
+    newdata,
+    xmax = 1000,
+    len = 1000,
+    add = F,
+    legend = NULL,
+    legend_title = NULL
+) {
+  
+  ## Get the three sets of parameters
+  mus <- model$out |> 
+    dplyr::filter(stringr::str_detect(term, "mu: ")) |> 
+    dplyr::pull(estimate)
+  alphas <- model$out|>
+    dplyr::filter(stringr::str_detect(term, "alpha: ")) |>
+    dplyr::pull(estimate)
+  thetas <- model$out |>
+    dplyr::filter(stringr::str_detect(term, "theta: ")) |>
+    dplyr::pull(estimate)
+  
+  ## Get the names of covariates
+  munames <- model$out |> 
+    dplyr::filter(stringr::str_detect(term, "mu: ")) |> 
+    dplyr::pull(term) 
+  alphanames <- model$out|>
+    dplyr::filter(stringr::str_detect(term, "alpha: ")) |>
+    dplyr::pull(term)
+  thetanames <- model$out |>
+    dplyr::filter(stringr::str_detect(term, "theta: ")) |>
+    dplyr::pull(term)
+  
+  ## Apply the names to the relevant parameters
+  names(mus) <- munames |> 
+    stringr::str_remove_all("mu: ")
+  names(alphas) <- alphanames |> 
+    stringr::str_remove_all("alpha: ")
+  names(thetas) <- thetanames |> 
+    stringr::str_remove_all("theta: ")
+  
+  ## Compute the parameter fits
+  if(length(mus) == 1) {
+    theMu <- exp(mus)
+  } else {
+    x1 <- cbind(1, newdata[, names(mus)[-1]])
+    theMu <- exp(sum(x1 * mus))
+  }
+  
+  if(length(alphas) == 1) {
+    theAlpha <- exp(alphas)
+  } else {
+    x2 <- cbind(1, newdata[, names(alphas)[-1]])
+    theAlpha <- exp(sum(x2 * alphas))
+  }
+  
+  if(length(thetas) == 1) {
+    theTheta <- exp(thetas)
+  } else {
+    x3 <- cbind(1, newdata[, names(thetas)[-1]])
+    theTheta <- exp(sum(x3 * thetas))
+  }
+  
+  ## Plot cdf the distribution
+  ll_plot(
+    theMu,
+    theAlpha,
+    theTheta,
+    xmax = xmax,
+    add = add,
+    len = len,
+    legend = legend,
+    legend_title = legend_title
+  )
+}
